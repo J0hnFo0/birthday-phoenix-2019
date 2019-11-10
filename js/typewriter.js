1b3fc9211d3c73a76b7916115b16d7fab351019b
JensErhardt <jenserhardt@gmail.com>
2019-11-10 14:48:00 +0100

    Changed id of typewriter.

--- js/typewriter.js
+++ js/typewriter.js
@@ -17,7 +17,7 @@ var iRow; // initialise current row
 function typewriter() {
   sContents = ' ';
   iRow = Math.max(0, iIndex - iScrollAt);
-  var destination = document.getElementById("typedtext");
+  var destination = document.getElementById("typewriter");
 
   while (iRow < iIndex) {
     sContents += aText[iRow++] + '<br />';
