f8860e222c7f0664ce6082d8658eccfbb4fc6919
JensErhardt <jenserhardt@gmail.com>
2019-11-10 14:13:56 +0100

    Added index.html. Added styles.scss and css output. Added script.js. Added firework.js. Added typewriter.js.

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/* Giftbox */
+
+window.onload=function(){
+  var merrywrap=document.getElementById("merrywrap");
+  var box=merrywrap.getElementsByClassName("giftbox")[0];
+  var step=1;
+  var stepMinutes=[2000,2000,1000,1000];
+  function init(){
+          box.addEventListener("click",openBox,false);
+  }
+  function stepClass(step){
+    merrywrap.className='merrywrap';
+    merrywrap.className='merrywrap step-'+step;
+  }
+  function openBox(){
+    if(step===1){
+      box.removeEventListener("click",openBox,false); 
+    }  
+    stepClass(step); 
+    if(step===3){ 
+    } 
+    if(step===4){ 
+       return;
+    }     
+    setTimeout(openBox,stepMinutes[step-1]);
+    step++;  
+  }
+   
+  init();
+ 
+}
+
+
+
+
+
+// Phoenix 
+
+// Canvas drawn
+// https://codepen.io/Vaddo/pen/niGJw
+
+// disappearing
+// https://codepen.io/cgorton/pen/BYKLQd
+
+// falling numbers
+// https://codepen.io/ThibaultJanBeyer/pen/JKYPZZ
